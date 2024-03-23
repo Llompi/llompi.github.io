677a4d0fe5e0b83286bedc1c42675bc093cf30fd
Llompi <joan.llbab@gmail.com>
2024-03-23 12:37:36 -0700

    New card base hovering project sections with theme toggle
    Re-worked the website structure.
    it is now focused on having the projects in their own individual sections that come into focus when hovering over them.
    Added a light and dark theme toggle

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-function toggleMenu() {
-    const nav = document.querySelector('.hamburger-nav');
-    nav.style.display = nav.style.display === 'none' ? 'block' : 'none';
-  }
-  
+window.addEventListener('load', () => {
+    const preloader = document.getElementById('preloader');
+    setTimeout(() => {
+        preloader.style.display = 'none';
+    }, 1000); // Pre-loading animation for 1 second
+});
+
+const toggleSwitch = document.getElementById('theme-toggle');
+const currentTheme = localStorage.getItem('theme') || 'light-theme';
+
+document.body.classList.add(currentTheme);
+toggleSwitch.checked = currentTheme === 'dark-theme';
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.body.classList.replace('light-theme', 'dark-theme');
+        localStorage.setItem('theme', 'dark-theme');
+    } else {
+        document.body.classList.replace('dark-theme', 'light-theme');
+        localStorage.setItem('theme', 'light-theme');
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
